b078684147762023ef01b660d15caa91ef298636
Bogdan Sukonnov <bogdansukonnov@gmail.com>
2019-07-20 10:25:11 +0300

    initial commit

---
+++ src/main/java/com/bogdansukonnov/socialnetwork/App.java
@@ -0,0 +1,13 @@
+package com.bogdansukonnov.socialnetwork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
